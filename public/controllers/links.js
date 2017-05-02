1177dc8bd7345b3c41297ed1b3fcf31e429cf7b6
mschlesk <markschleske@gmail.com>
2017-05-02 15:49:04 -0600

    Initialize links controller.

--- public/controllers/links.js
+++ public/controllers/links.js
@@ -1,6 +1,14 @@
 angular.module('musicApp')
+.controller('linksController', function() {
+
+})
 .directive('links', function() {
   return {
+    scope: {
+      links: '<'
+    },
+    controllerAs: 'ctrl',
+    controller: 'linksController',
     templateUrl: 'views/links.ejs'
   }
 });
