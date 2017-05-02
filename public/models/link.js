48528d2beaded763c85353145fd51946bffd8e28
mschlesk <markschleske@gmail.com>
2017-05-02 00:40:59 -0600

    Add minimal schema properties.

--- public/models/link.js
+++ public/models/link.js
@@ -2,7 +2,12 @@ var mongoose = require('mongoose');
 var db = require('../../db/config');
 
 var linkSchema = mongoose.Schema({
-
+  url: String,
+  title: String,
+  // artist: String,
+  // album: String,
+  // host: String,
+  dateCreated: { type: Date, default: Date.now }
 });
 
 var Link = mongoose.model('Link', linkSchema);
