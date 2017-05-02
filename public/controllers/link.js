7de8bbd705c3da0a759e7ea87d62f8013cafe310
mschlesk <markschleske@gmail.com>
2017-05-02 17:54:26 -0600

    Pass link object down from links component to link component.

--- public/controllers/link.js
+++ public/controllers/link.js
@@ -1,6 +1,14 @@
 angular.module('musicApp')
 .directive('linkItem', function() {
   return {
+    scope: {
+      link: '<'
+    },
+    controllerAs: 'linkItemCtrl',
+    bindToController: true,
+    controller: function() {
+      console.log(this);
+    },
     templateUrl: 'views/link.ejs'
   }
 });
