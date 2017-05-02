0759de40e8413d36c3ed223834fd04e74ab3abc4
mschlesk <markschleske@gmail.com>
2017-05-02 15:47:54 -0600

    Initialize app controller.

--- public/controllers/app.js
+++ public/controllers/app.js
@@ -2,7 +2,11 @@ angular.module('musicApp', [])
 .directive('app', function(){
   // Runs during compile
   return {
-    // controller: function($scope, $element, $attrs, $transclude) {},
+    bindToController: true,
+    controllerAs: 'appCtrl',
+    controller: function($scope, $http) {
+      console.log($scope);
+    },
     // restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
     templateUrl: '/views/app.ejs'
   };
