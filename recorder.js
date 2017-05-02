c3e780e75fe4fc205f2f4081acf2ed42cb4595ac
mschlesk <markschleske@gmail.com>
2017-05-02 11:47:20 -0600

    Parse request body using body-parser module.

--- recorder.js
+++ recorder.js
@@ -1,7 +1,8 @@
 var db = require('./db/config');
 var Link = require('./public/models/link');
-
 var apiRoutes = require('./utils/apiRoutes');
+
+var bodyparser = require('body-parser');
 
 var express = require('express');
 var app = express();
@@ -9,6 +10,9 @@ var app = express();
 app.set('views', 'public/views');
 app.set('view engine', 'ejs');
 
+// Parse request body
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
