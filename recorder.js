88d737cafb9758f932f9650aa0031a497b2d5dab
mschlesk <markschleske@gmail.com>
2017-05-01 18:30:01 -0600

    Refactor to use new 'public' subdirectory.

--- recorder.js
+++ recorder.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.set('views', 'views');
+app.set('views', 'public/views');
 app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
   res.render('index');
 });
