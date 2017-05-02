ede11ea108afd665d17c9a3ff2a3d8fd45ad9386
mschlesk <markschleske@gmail.com>
2017-05-01 18:01:40 -0600

    Serve index.ejs on get request root.

--- recorder.js
+++ recorder.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
-
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+  res.render('index');
+});
 
 module.exports = app;
