58ec02975f27da3e2da3f624d1bae614c3a2b1ef
mschlesk <markschleske@gmail.com>
2017-05-02 00:16:18 -0600

    Log database connection status.

--- db/config.js
+++ db/config.js
@@ -2,3 +2,14 @@ var mongoose = require('mongoose');
 
 databaseURL = 'mongodb://localhost:27017/recorderdb';
 mongoose.connect(databaseURL);
+
+var db = mongoose.connection;
+
+db.on('error', function(error) {
+  console.error(`FAILURE: Failed to connect to database at ${databaseURL}
+    Error: ${error}`);
+});
+
+db.on('open', function() {
+  console.log(`SUCCESS: Successfully connected to database at ${databaseURL}`);
+});
